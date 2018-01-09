bc566f431ece1f2a6afbefc8d978b9bce0d46475
pierrelaffite <pierre.laffite.mata@gmail.com>
2018-01-09 16:55:35 +0100

    changement signature evaluate

--- source/algorithmes/algoInterface.java
+++ source/algorithmes/algoInterface.java
@@ -5,6 +5,6 @@ public interface algoInterface {
 	public Object importer(String file);
 	public Object fit(Object o);
 	public Object[] split(Object o);
-	public void evaluate(Object o);
+	public void evaluate(Object model, Object test);
 
 }
