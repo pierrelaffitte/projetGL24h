2dca720682fbd189a6cb03b6722a4943b71ed1f6
flav <flav@Flav02>
2018-01-09 14:18:49 +0100

    wka

--- source/algorithmes/Weka.java
+++ source/algorithmes/Weka.java
@@ -1,15 +1,1 @@
 package algorithmes;
-import weka.core.Instances;
-import weka.core.converters.ConverterUtils.DataSource;
-
-public class Weka {
-
-	 public static void main(String[] args) throws Exception {
-		DataSource source = new DataSource("resources/iris.csv");
-		Instances data = source.getDataSet();
-		for (int i =0; i < data.size(); i++ ) {
-			System.out.println(data.get(i));
-		}
-	}
-
-}
