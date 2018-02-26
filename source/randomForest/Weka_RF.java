ce93ad4740df2a0448ddbc1bf0d2761fb8bc97e2
pierre laffitte <pierre.laffitte.mata@gmail.com>
2018-02-26 11:55:03 +0100

    Création de la classe Random Forest pour Weka

--- source/randomForest/Weka_RF.java
+++ source/randomForest/Weka_RF.java
@@ -1,4 +1,4 @@
-package classificationTree;
+package randomForest;
 
 import java.util.Enumeration;
 
@@ -14,7 +14,7 @@ import weka.classifiers.trees.*;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.NumericToNominal;
 
-public class Weka_CT implements algoInterface {
+public class Weka_RF implements algoInterface {
 
 	public Object importer(String file) {
 		Instances data = null;
@@ -47,27 +47,8 @@ public class Weka_CT implements algoInterface {
 		return pos;
 	}
 
-	/*	public Instances removeColumn(Instance data, int col) {
-		NumericToNominal convert= new NumericToNominal();
-		String[] options= new String[2];
-		options[0]="-R";
-		options[1]= String.valueOf(pos);  //range of variables to make numeric
-		System.out.println(options[1]);
-		try {
-			weka.test(data);
-			convert.setOptions(options);
-			convert.setInputFormat(data);
-			res = Filter.useFilter(data, convert);
-			// on s'assure que la conversion est bien faite
-			Instance row = res.get(1);
-			System.out.println(row.attribute(pos).isString());
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}*/
-
 	public static boolean isString(Instances data, String y) {
-		Weka_CT weka = new Weka_CT();
+		Weka_RF weka = new Weka_RF();
 		Instance row = data.get(0);
 		int pos = weka.posY(data, y);
 		System.out.println(row.attribute(pos).isNumeric());
@@ -75,16 +56,16 @@ public class Weka_CT implements algoInterface {
 	}
 
 	public Object fit(String train, String y,String...args){
-		J48 tree = null;
+		RandomForest tree = null;
 		try {
 			Instances train1 = ((Instances) importer(train));
-			if (!Weka_CT.isString(train1, y) ){
+			if (!Weka_RF.isString(train1, y) ){
 				System.err.println("do conversion");
 				train1 = this.convertNumToString(train1, y);
 			}
 			train1.setClassIndex(posY(train1,y));
 			//train1.setClassIndex(train1.numAttributes()-1);
-			tree = new J48();
+			tree = new RandomForest();
 			tree.buildClassifier(train1);
 		}catch (Exception e) {
 			e.printStackTrace();
@@ -94,7 +75,7 @@ public class Weka_CT implements algoInterface {
 	}
 
 	public  Instances convertNumToString(Instances data, String y) {
-
+		/* change la variable y de quantitative à qualitative*/
 		Instances res = null;
 		int pos = this.posY(data, y);
 		System.err.println(pos);
@@ -142,7 +123,7 @@ public class Weka_CT implements algoInterface {
 	public Object evaluate(String train, String test, String y,String...args) {
 		//Object model=fit(train,y);
 		Object resultat = null;
-		J48 tree = (J48) fit(train,y);
+		RandomForest tree = (RandomForest) fit(train,y);
 		Instances test1 = (Instances) importer(test);
 		if (!this.isString(test1, y) ){
 			test1 = this.convertNumToString(test1, y);
@@ -165,69 +146,8 @@ public class Weka_CT implements algoInterface {
 	public static void main(String[] args) throws Exception {
 		int fold = 10; // nombre k de groupes pour la cross validation
 		int seed = 1; //graine pour la reproductibilité des résultats
-
-		/*
-		String file = "resources/iris.csv";
-		Weka weka = new Weka();
-		Instances data = weka.importer(file);
-		data.setClassIndex(data.numAttributes()-1);
-
-		// le choix du modele : J48 = C4.5
-		// http://weka.sourceforge.net/doc.dev/weka/classifiers/trees/package-summary.html
-		J48 tree = new J48();
-
-		// on mélange notre jeu de données
-		Random rand = new Random(seed);
-		Instances randData = new Instances(data);
-		randData.randomize(rand);
-		if (randData.classAttribute().isNominal())
-			randData.stratify(fold);
-		double averagecorrect = 0;
-
-		// split train and test
-		System.out.println(randData.size());
-		Instances train = randData.trainCV(5, 4);
-		Instances test = randData.testCV(5, 4);		
-		System.out.println(train);
-		System.out.println("\n\n\n");
-		System.out.println(test);
-		System.out.println("\n\n\n");
-
-
-		// CV
-		for (int n=0;n<fold;n++){
-			Evaluation eval = new Evaluation(randData);
-			Instances app = train.trainCV(fold, n); // construit train sur data sauf nieme fold
-			Instances val = train.testCV(fold, n); // construit test sur data avec nieme fold
-
-			tree.buildClassifier(app);
-			eval.evaluateModel(tree, val);
-			double correct = eval.pctCorrect();
-			averagecorrect = averagecorrect + correct;
-			System.out.println("the "+n+"th cross validation:"+eval.toSummaryString());
-		}
-		System.out.println("the average correction rate of "+fold+" cross validation: "+averagecorrect/fold);
-		 */
-
-		Weka_CT weka = new Weka_CT();
-		//J48 tree = (J48) weka.fit("resources/train_iris.csv","Species");
-		// evaluate on test echantillon
-		//weka.evaluate(tree, "resources/test_iris.csv","Species");
-		/*
-		Instances data = (Instances)weka.importer("resources/train_winequality.csv");
-		Instance row = data.get(1);
-		int pos = weka.posY(data, "quality");
-		System.out.println(row.attribute(pos).isString());
-		 */
-
-		/*
-		 * Instances data = (Instances)weka.importer("resources/train_winequality.csv");
-		 */
-		/*
-		System.out.println(weka.posY(data, "quality"));
-		System.out.println(data.get(11));
-		Weka.isString(data, "quality");*/
-		weka.evaluate("resources/train_winequality.csv","resources/test_winequality.csv","quality");
+		Weka_RF weka = new Weka_RF();
+		System.out.println(weka.evaluate("resources/train_iris.csv","resources/test_iris.csv","Species"));
 	}
 
 }
