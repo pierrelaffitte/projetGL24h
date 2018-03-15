e10b1fcce286b05d156eb4c690adb44ba738a22d
charleneNoe <noe.charlene@hotmail.fr>
2018-03-15 12:02:23 +0100

    formulaire

--- source/servlet/Test.java
+++ source/servlet/Test.java
@@ -9,23 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="index", urlPatterns={"/index"})
+@WebServlet(name="info", urlPatterns={"/Info"})
 public class Test extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		PrintWriter p = new PrintWriter(resp.getOutputStream());
-		p.print("Hello world bbbbbbbb");
-		p.flush();
+		response.setContentType("text/html");
 
+		PrintWriter out = response.getWriter();
+
+		out.println("<HTML>\n<BODY>\n" +
+				"<H1>Recapitulatif des informations</H1>\n" +
+				"<UL>\n" +            
+				" <LI>Nom: "
+				+ request.getParameter("name") + "\n" +
+				" <LI>Prenom: "
+				+ request.getParameter("firstname") + "\n" +
+				" <LI>Age: "
+				+ request.getParameter("age") + "\n" +
+				"</UL>\n" +                
+				"</BODY></HTML>");
 	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
-	}    
 }
